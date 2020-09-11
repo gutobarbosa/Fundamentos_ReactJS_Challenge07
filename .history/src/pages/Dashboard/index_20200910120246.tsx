719b828fdc50a9c9be8fbb0fd82ef92d3cb1e674
Augusto Barbosa <augusto_barbosa_sp@hotmail.com>
2020-09-11 13:36:26 -0300

    Challenge 07 finished

---
+++ .history/src/pages/Dashboard/index_20200910120246.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+import { title } from 'process';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: string;
+  outcome: string;
+  total: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  // const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const [transaction] = await Promise.all([api.get('transactions')]);
+      // console.log(transaction.data);
+      setTransactions(transaction.data.transactions);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">R$ 4000,00</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => (
+                <tr>
+                  <td className="title">{transaction.title}</td>
+                  <td className="income">{`R$ ${transaction.value}`}</td>
+                  <td>category</td>
+                  <td>{transaction.created_at}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ .history/src/pages/Dashboard/index_20200910131809.tsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+
+import { title } from 'process';
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: string;
+  outcome: string;
+  total: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const transaction = await api.get('transactions');
+      // console.log(transaction.data);
+      setTransactions(transaction.data.transactions);
+      setBalance(transaction.data.balance);
+      console.log(transaction.data.transactions);
+
+      const formatedTransaction = transaction.data.transactions.map((transact: Transaction) => ({
+          ...transact,
+          formattedValue: formatValue(transact.value),
+          formattedDate: new Date(transact.created_at).toLocaleDateString(
+            'pt-br',
+          ),
+        }),
+      );
+
+      setTransactions(formatedTransaction);
+    }
+
+    loadTransactions();
+  }, []);
+  console.log(transactions);
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">{`R$ ${balance.income}`}</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">{`R$ ${balance.outcome}`}</h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">{`R$ ${balance.total}`}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => (
+                <tr>
+                  <td className="title">{transaction.title}</td>
+                  <td className="income">{`R$ ${transaction.value}`}</td>
+                  <td>categoria</td>
+                  <td>{transaction.created_at}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
